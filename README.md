171a796488989231055c702a1fd68ced4bf79bb6
ArthurFDLR <arthfind@gmail.com>
2022-09-15 23:02:02 -0500

    Add CSV queue & Notes about setup

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Instagram Post Scheduler
+
+## Setup
+
+- Create private S3 Bucket 
+- Create public S3 Bucket
+  - Allow publicj during creation;
+  - Add permision:
+    ```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPublicRead",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:GetObject",
+            "Resource": [
+                "arn:aws:s3:::instagram-post-scheduler-public/*",
+                "arn:aws:s3:::instagram-post-scheduler-public"
+            ]
+        }
+    ]
+}
+    ```
+- create Lambda function with Python runtime
+- add S3 permissions:
+    Configuration > Permissions > Role Name > Add permissions > Attach policy > Select AmazonS3FullAccess > Attach Policies
+- Create test case (Ctrl+Shift+C) with event JSON
+    ```
+    {
+        "type": "test"
+    }
+    ```
+- Package the application i.e. run ./packaging.sh.
+- Upload into Lambda function (see ./dist/): Code > Upload from > .zip file
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,15 +1,16 @@
 import json, requests, boto3, os
 from datetime import datetime, timedelta
+from pathlib import Path
+import pandas as pd
 
 from graphapi import GraphAPI
-
 
 
 SNS_CLIENT = boto3.client('sns')
 S3_CLIENT = boto3.client('s3')
 S3_RES = boto3.resource('s3')
 
-S3_BUCKET_NAME = "thisnightskydoesnotexist"
+S3_BUCKET_NAME = "instragram-post-scheduler"
 APP_PARAM_JSON_NAME = "app_parameters.json"
 GRAPHAPI_PARAM_JSON_NAME = "GraphAPI_parameters.json"
 
@@ -33,11 +34,39 @@ def get_image_url(params_data):
     return image_url
 
 def send_sns(msg:str, subject:str="💥WARNING: ThisNightSkyDoesNotExist encountered a problem"):
+    print(msg)
     SNS_CLIENT.publish(
         TargetArn=os.environ['snsARN'],
         Subject=subject,
         Message=msg
     )
+
+
+class QueueCSV():
+    def __init__(self, csv_queue_path: Path) -> None:
+        self.csv_queue_path = csv_queue_path
+        self.queue_df = pd.read_csv(csv_queue_path)
+
+    def peek(self):
+        if self._is_empty():
+            return None
+        top_row = self.queue_df[self.queue_df.status == False].iloc[0]
+        return dict(photo_url=top_row.photo_url, description=top_row.description)
+    
+    def pop(self):
+        if self._is_empty():
+            return None
+        top_row_id = self.queue_df[self.queue_df.status == False].iloc[0].name
+        top_row = self.queue_df.iloc[top_row_id]
+        self.queue_df.at[top_row_id, 'status'] = True
+        self.queue_df.to_csv(self.csv_queue_path, index=False)
+        return dict(photo_url=top_row.photo_url, description=top_row.description)
+
+    def _is_empty(self):
+        return self.queue_df.status.all()
+
+    def __len__(self):
+        return int((~self.queue_df.status).sum())
 
     
 def lambda_handler(event, context):
@@ -64,16 +93,29 @@ def lambda_handler(event, context):
     # )['Body'].read())
     # # print(json.dumps(app_params, indent=4))
 
-    # # Initilize GraphAPI
-    # try:
-    #     graph_api = GraphAPI(graphapi_params)
-    # except Exception as e:
-    #     send_sns(msg="")
-    #     print(e)
-    #     return {
-    #         'statusCode': 500,
-    #         'body': json.dumps({"error": str(e)})
-    #     }
+    # Initilize GraphAPI
+    try:
+        graph_api = GraphAPI(graphapi_params)
+    except Exception as e:
+        send_sns(msg="")
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({"error": str(e)})
+        }
+
+    # Check for expiration date
+    remaining_time_token = graph_api.expiration_date - datetime.now()
+    if remaining_time_token < timedelta(days=10):
+        send_sns(msg=f"`access_token` expires on {graph_api.expiration_date}. Please renew the GraphAPI access token.")
+
+    # Check image availability for the 5 days to come
+    # for day_id in range(5):
+    #     if not check_url_exists(get_image_url(app_params['data'])):
+    #         send_sns(msg=f"The image database will run out of content in {day_id+1} day{'s' if day_id>0 else ''}.")
+    #         break
+    #     app_params['data']["name_variable"] = data_name_variable_update(app_params['data']["name_variable"])
+
 
     # # Build image URL
     # image_url = get_image_url(app_params["data"])
@@ -91,11 +133,6 @@ def lambda_handler(event, context):
     #         'body': json.dumps({"error": "Content posting failed"})
     #     }
 
-    # # Check for expiration date
-    # remaining_time_token = graph_api.expiration_date - datetime.now()
-    # if remaining_time_token < timedelta(days=10):
-    #     send_sns(msg=f"`access_token` expires on {graph_api.expiration_date}. Please renew the GraphAPI access token.")
-
     # # Update image counter in parameter file
     # app_params['data']["name_variable"] = data_name_variable_update(app_params['data']["name_variable"])
     # S3_RES.Object(S3_BUCKET_NAME, APP_PARAM_JSON_NAME).delete()
@@ -105,12 +142,6 @@ def lambda_handler(event, context):
     #     Key=APP_PARAM_JSON_NAME,
     # )
 
-    # # Check image availability for the 5 days to come
-    # for day_id in range(5):
-    #     if not check_url_exists(get_image_url(app_params['data'])):
-    #         send_sns(msg=f"The image database will run out of content in {day_id+1} day{'s' if day_id>0 else ''}.")
-    #         break
-    #     app_params['data']["name_variable"] = data_name_variable_update(app_params['data']["name_variable"])
     
     logger_body = {
         "GraphAPI parameters": graphapi_params,
